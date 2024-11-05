f9ade261e69a8d3223158baf367d167912da43a0
killergot <disney.132435morozko@mail.ru>
2024-11-05 22:29:31 +0300

    поменял ключ нужно, конечно, спрятать его))

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ HELP_COMMAND : str = """
 <b>/links</b> - <em>Позволяет добавить фото</em>
 <b>/cancel</b> - <em>отменяет действие</em>
 """
-ID_MY_GIRL = 813608578
-MY_ID = 588564125
+ID_MY_GIRL : int = 813608578
+MY_ID : int = 588564125
 
-TOKEN_ADMIN : str = '6695234791:AAG4Y9ZHh9rRmcyAwghFob1ErjHlZ_rIDXk'
+TOKEN_ADMIN : str = '6668418273:AAEA2Lm_UGvKboGtoXoEqgOfC9v0LwRYbgQ'
 TOKEN_OWM : str = '381daa25d46266525eee2ff2281df295'
 
 
